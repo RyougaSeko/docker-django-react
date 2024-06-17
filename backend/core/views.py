4c1e8516c498d8b7993a1d32aff8003ad0df91b3
hoop105ryouga <hoop105ryouga@icloud.com>
2024-06-17 15:03:21 +0900

    edit readme

--- backend/core/views.py
+++ backend/core/views.py
@@ -27,3 +27,21 @@ class ItemViewSet(viewsets.ModelViewSet):
         # 例: メールを送信するなど
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        print('destroy')
+        instance = self.get_object()  # URLのキーパラメータから自動的にオブジェクトを取得する
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)  # partial 引数が True の場合は、部分的な更新になる
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {} # キャッシュをクリア
+
+        return Response(serializer.data)
--- frontend/src/pages/sample.jsx
+++ frontend/src/pages/sample.jsx
@@ -1,32 +1,138 @@
 import React, { useState, useEffect } from 'react';
-import '../App.css';
+import { API_BASE_URL } from '../constants';
 
-function App() {
+function TodoApp() {
   const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState({ name: '', description: '' });
 
   useEffect(() => {
-    // # TODO: 定数でURLを管理する
-    fetch('http://localhost:8080/api/items/')
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(data => setItems(data))
-      .catch(error => console.error('Error fetching items:', error));
+    fetchItems();
   }, []);
 
+  const fetchItems = () => {
+    fetch(`${API_BASE_URL}/items/`)
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.error('Error fetching items:', error));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewItem(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const createItem = () => {
+    fetch(`${API_BASE_URL}/items/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newItem),
+    })
+    .then(response => response.json())
+    .then(() => {
+      fetchItems();
+      setNewItem({ name: '', description: '' }); // フォームをリセット
+    })
+    .catch(error => console.error('Error creating item:', error));
+  };
+
+  const handleItemChange = (e, id) => {
+    const { name, value } = e.target;
+    setItems(items.map(item => item.id === id ? { ...item, [name]: value } : item));
+  };
+
+  const saveItem = (id) => {
+    const item = items.find(item => item.id === id);
+    fetch(`${API_BASE_URL}/items/${id}/`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(item),
+    })
+    .then(response => response.json())
+    .then(() => fetchItems())
+    .catch(error => console.error('Error updating item:', error));
+  };
+
+  const deleteItem = (id) => {
+    // DELETEリクエストを送るための関数
+    fetch(`${API_BASE_URL}/items/${id}/`, {
+      method: 'DELETE',
+    })
+    .then(() => fetchItems()) // アイテムを再取得
+    .catch(error => console.error('Error deleting item:', error));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Items</h1>
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>{item.name}｜{item.description}</li>
-          ))}
-        </ul>
+    <div className="container mx-auto mt-10">
+      <header className="text-center">
+        <h1 className="text-4xl font-bold mb-6">SAMPLE TODO APP</h1>
+        <div className="flex justify-center gap-4 mb-6">
+          <input
+            type="text"
+            name="name"
+            value={newItem.name}
+            onChange={handleInputChange}
+            placeholder="タスク"
+            className="form-input px-4 py-2 border rounded"
+          />
+          <input
+            type="text"
+            name="description"
+            value={newItem.description}
+            onChange={handleInputChange}
+            placeholder="詳細"
+            className="form-input px-4 py-2 border rounded"
+          />
+          <button onClick={createItem} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            アイテムを作成
+          </button>
+        </div>
+        <table className="table-auto w-full">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="px-4 py-2">タスク</th>
+              <th className="px-4 py-2">詳細</th>
+              <th className="px-4 py-2">操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(item => (
+              <tr key={item.id} className="text-center">
+                <td className="border px-4 py-2">
+                  <input
+                    type="text"
+                    name="name"
+                    value={item.name}
+                    onChange={(e) => handleItemChange(e, item.id)}
+                    className="form-input px-4 py-2 border rounded"
+                  />
+                </td>
+                <td className="border px-4 py-2">
+                  <input
+                    type="text"
+                    name="description"
+                    value={item.description}
+                    onChange={(e) => handleItemChange(e, item.id)}
+                    className="form-input px-4 py-2 border rounded"
+                  />
+                </td>
+                <td className="border px-4 py-2">
+                  <button onClick={() => saveItem(item.id)} className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded mr-2">
+                    保存
+                  </button>
+                  <button onClick={() => deleteItem(item.id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded">
+                    完了
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
 }
 
-export default App;
+export default TodoApp;
